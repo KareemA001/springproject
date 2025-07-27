4881855cff8e0615401846a25935becdd5fe59e8
KareemA001 <kalaa8851@gmail.com>
2025-07-28 00:59:37 +0300

    init commit

--- src/main/java/com/example/springjourney/project/controller/ContactController.java
+++ src/main/java/com/example/springjourney/project/controller/ContactController.java
@@ -1,13 +1,35 @@
 package com.example.springjourney.project.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
 
+    private static Logger log = LoggerFactory.getLogger(ContactController.class) ;
     @RequestMapping("/contact")
     public String contact(){
         return "contact.html";
     }
+
+    @RequestMapping(value = "/saveMsg",method = POST)
+    public ModelAndView saveMsg(@RequestParam String name,@RequestParam String mobileNum
+                               ,@RequestParam String email,@RequestParam String subject
+                               ,@RequestParam String message){
+        log.info("Name "+name);
+        log.info("MobileNumber "+mobileNum);
+        log.info("Email "+email);
+        log.info("subject "+subject);
+        log.info("message "+message);
+
+        return new ModelAndView("redirect:/contact") ;
+    }
+
 }
