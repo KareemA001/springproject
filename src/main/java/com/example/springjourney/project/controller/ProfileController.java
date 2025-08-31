7e73c133755301b37bfcc15de3e77faa3983016f
KareemA001 <kalaa8851@gmail.com>
2025-09-01 02:59:24 +0300

    init commit

--- src/main/java/com/example/springjourney/project/controller/ProfileController.java
+++ src/main/java/com/example/springjourney/project/controller/ProfileController.java
@@ -1,20 +1,30 @@
 package com.example.springjourney.project.controller;
 
 
+import com.example.springjourney.project.model.Address;
 import com.example.springjourney.project.model.Person;
 import com.example.springjourney.project.model.Profile;
+import com.example.springjourney.project.repos.PersonRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Data
 @Controller
 public class ProfileController {
 
-    @RequestMapping(path="displayProfile")
+    @Autowired
+    private PersonRepository personRepository;
+
+    @RequestMapping(path="/displayProfile")
     public ModelAndView displayProfile(Model model, HttpSession session) {
         Person person = (Person) session.getAttribute("loggedInPerson");
         Profile profile = new Profile();
@@ -32,4 +42,29 @@ public class ProfileController {
         modelAndView.addObject("profile",profile);
         return modelAndView;
     }
+
+    @RequestMapping(name="/updateProfile", method= RequestMethod.POST)
+    public String pdateProfile(@Valid @ModelAttribute("person") Profile profile, Errors errors , HttpSession session) {
+        if(errors.hasErrors()){
+            return "profile.html";
+        }
+
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        person.setName(profile.getName());
+        person.setEmail(profile.getEmail());
+        person.setMobileNumber(profile.getMobileNumber());
+        if(person.getAddress() ==null || !(person.getAddress().getAddressId()>0)){
+            person.setAddress(new Address());
+        }
+        person.getAddress().setAddress1(profile.getAddress1());
+        person.getAddress().setAddress2(profile.getAddress2());
+        person.getAddress().setCity(profile.getCity());
+        person.getAddress().setState(profile.getState());
+        person.getAddress().setZipCode(profile.getZipCode());
+        Person savedPerson = personRepository.save(person);
+        session.setAttribute("loggedInPerson", savedPerson);
+        return "redirect:/displayProfile";
+
+    }
+
 }
